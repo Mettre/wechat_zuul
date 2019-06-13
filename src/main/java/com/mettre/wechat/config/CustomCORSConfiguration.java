c2f6a94a824363c91f1efefb408e537e8f6836ba
Mettre <1052461392@qq.com>
2019-06-13 08:32:58 +0800

    微信网关项目初始化提交

---
+++ src/main/java/com/mettre/wechat/config/CustomCORSConfiguration.java
@@ -0,0 +1,26 @@
+package com.mettre.wechat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Spring Boot跨域
+ */
+public class CustomCORSConfiguration {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
